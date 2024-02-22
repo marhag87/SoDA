8440155fedfbcefa28c63b6fc8dfb4552497dd44
Martin Hagström <marhag87@gmail.com>
2024-02-22 22:15:46 +0100

    Sort characters by level

--- SoDA/Gui.lua
+++ SoDA/Gui.lua
@@ -11,7 +11,8 @@ function SoDA:Gui()
     local width = (numChararcters * 120) + 40
     f:SetWidth(width)
 
-    for guid,character in pairs(self.db.global.characters) do
+    local characters = self.db.global.characters
+    for guid,character in SoDA:spairs(characters, function(t, a, b) return t[b].basic.level < t[a].basic.level end) do
         local group = self.aceGui:Create("SimpleGroup")
         group:SetWidth(120)
 
