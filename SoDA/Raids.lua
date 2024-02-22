686cbc2e5ec89688d990115bf2e70639c7a58c08
Martin Hagström <marhag87@gmail.com>
2024-02-22 20:54:16 +0100

    Add blackfathom deeps

--- SoDA/Raids.lua
+++ SoDA/Raids.lua
@@ -4,6 +4,14 @@ function SoDA:GetRaids()
     if numSavedInstances > 0 then
         for i=1, numSavedInstances do
             local name, lockoutId, reset, difficultyId, locked, extended, instanceIDMostSig, isRaid, maxPlayers, difficultyName, numEncounters, encounterProgress, extendDisabled, instanceId = GetSavedInstanceInfo(i)
+            if instanceId == 48 then -- Blackfathom Deeps
+                local bfd = {}
+                bfd.resetAt = time() + reset
+                bfd.encounterProgress = encounterProgress
+                bfd.numEncounters = numEncounters
+                bfd.name = name
+                raids.bfd = bfd
+            end
             if instanceId == 90 then -- Gnomeregan
                 local gnomeregan = {}
                 gnomeregan.resetAt = time() + reset
@@ -20,6 +28,12 @@ end
 function SoDA:GetRaidsGui(character)
     local group = self.aceGui:Create("SimpleGroup")
 
+    local bfd = {
+        ["encounterProgress"] = "?",
+        ["numEncounters"] = 7,
+        ["name"] = "Blackfathom Deeps",
+        ["resetAt"] = 0,
+    }
     local gnomeregan = {
         ["encounterProgress"] = "?",
         ["numEncounters"] = 6,
@@ -34,12 +48,30 @@ function SoDA:GetRaidsGui(character)
                 gnomeregan.encounterProgress = "?"
             end
         end
+        if character.raids.bfd ~= nil then
+            bfd = character.raids.bfd
+            if time() > bfd.resetAt then
+                bfd.encounterProgress = "?"
+            end
+        end
     end
 
     -- Header
     local currencyHeader = self.aceGui:Create("Label")
     currencyHeader:SetText("Raids")
     group:AddChild(currencyHeader)
+
+    -- Blackfathom Deeps
+    local bfdLock = self.aceGui:Create("Label")
+    if bfd.encounterProgress == bfd.numEncounters and time() < bfd.resetAt then
+        bfdLock:SetColor(0, 1, 0)
+    end
+    if bfd.encounterProgress == "?" then
+        bfdLock:SetText(bfd.name)
+    else
+        bfdLock:SetText(bfd.name .. " " .. bfd.encounterProgress .. "/" .. bfd.numEncounters)
+    end
+    group:AddChild(bfdLock)
 
     -- Gnomeregan
     local gnomereganLock = self.aceGui:Create("Label")
