b7c9ebc9f762c8acbed1b3a1ce047195468828e2
Martin Hagström <marhag87@gmail.com>
2024-02-22 21:04:20 +0100

    Simplify raid gui

--- SoDA/Raids.lua
+++ SoDA/Raids.lua
@@ -28,62 +28,46 @@ end
 function SoDA:GetRaidsGui(character)
     local group = self.aceGui:Create("SimpleGroup")
 
-    local bfd = {
-        ["encounterProgress"] = "?",
-        ["numEncounters"] = 7,
-        ["name"] = "Blackfathom Deeps",
-        ["resetAt"] = 0,
-    }
-    local gnomeregan = {
-        ["encounterProgress"] = "?",
-        ["numEncounters"] = 6,
-        ["name"] = "Gnomeregan",
-        ["resetAt"] = 0,
-    }
-
-    if character.raids ~= nil then
-        if character.raids.gnomeregan ~= nil then
-            gnomeregan = character.raids.gnomeregan
-            if time() > gnomeregan.resetAt then
-                gnomeregan.encounterProgress = "?"
-            end
-        end
-        if character.raids.bfd ~= nil then
-            bfd = character.raids.bfd
-            if time() > bfd.resetAt then
-                bfd.encounterProgress = "?"
-            end
-        end
-    end
-
     -- Header
     local currencyHeader = self.aceGui:Create("Label")
     currencyHeader:SetText("Raids")
     group:AddChild(currencyHeader)
 
     -- Blackfathom Deeps
-    local bfdLock = self.aceGui:Create("Label")
-    if bfd.encounterProgress == bfd.numEncounters and time() < bfd.resetAt then
-        bfdLock:SetColor(0, 1, 0)
-    end
-    if bfd.encounterProgress == "?" then
-        bfdLock:SetText(bfd.name)
-    else
-        bfdLock:SetText(bfd.name .. " " .. bfd.encounterProgress .. "/" .. bfd.numEncounters)
-    end
+    local bfd = character.raids.bfd or {
+        ["encounterProgress"] = nil,
+        ["numEncounters"] = 7,
+        ["name"] = "Blackfathom Deeps",
+        ["resetAt"] = 0,
+    }
+    local bfdLock = SoDA:RaidLock(bfd)
     group:AddChild(bfdLock)
 
     -- Gnomeregan
-    local gnomereganLock = self.aceGui:Create("Label")
-    if gnomeregan.encounterProgress == gnomeregan.numEncounters and time() < gnomeregan.resetAt then
-        gnomereganLock:SetColor(0, 1, 0)
-    end
-    if gnomeregan.encounterProgress == "?" then
-        gnomereganLock:SetText(gnomeregan.name)
-    else
-        gnomereganLock:SetText(gnomeregan.name .. " " .. gnomeregan.encounterProgress .. "/" .. gnomeregan.numEncounters)
-    end
+    local gnomeregan = character.raids.gnomeregan or {
+        ["encounterProgress"] = nil,
+        ["numEncounters"] = 6,
+        ["name"] = "Gnomeregan",
+        ["resetAt"] = 0,
+    }
+    local gnomereganLock = SoDA:RaidLock(gnomeregan)
     group:AddChild(gnomereganLock)
 
     return group
+end
+
+function SoDA:RaidLock(raid)
+    local raidLock = self.aceGui:Create("Label")
+    if time() > raid.resetAt then
+        raid.encounterProgress = nil
+    end
+    if raid.encounterProgress == raid.numEncounters and time() < raid.resetAt then
+        raidLock:SetColor(0, 1, 0)
+    end
+    if raid.encounterProgress == nil then
+        raidLock:SetText(raid.name)
+    else
+        raidLock:SetText(raid.name .. " " .. raid.encounterProgress .. "/" .. raid.numEncounters)
+    end
+    return raidLock
 end
