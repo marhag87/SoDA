6c80c920a300c603e0fb316e691cc04f2df8663f
Martin Hagström <marhag87@gmail.com>
2024-03-17 12:10:08 +0100

    Use defaults for localization

--- SoDA/Locale/enUS.lua
+++ SoDA/Locale/enUS.lua
@@ -3,80 +3,80 @@ local L = LibStub("AceLocale-3.0"):NewLocale("SoDALocale", "enUS")
 if not L then return end
 
 -- General
-L["Resets in"] = "Resets in"
-L["Alliance"] = "Alliance"
-L["Horde"] = "Horde"
+L["Resets in"] = true
+L["Alliance"] = true
+L["Horde"] = true
 
 -- Basic
-L["Basic"] = "Basic"
-L["Realm"] = "Realm"
-L["Level"] = "Level"
-L["Mount"] = "Mount"
-L["Sleeping bag"] = "Sleeping bag"
-L["Cooldown remaining"] = "Cooldown remaining"
-L["Buff"] = "Buff"
-L["Rested"] = "Rested"
+L["Basic"] = true
+L["Realm"] = true
+L["Level"] = true
+L["Mount"] = true
+L["Sleeping bag"] = true
+L["Cooldown remaining"] = true
+L["Buff"] = true
+L["Rested"] = true
 
 -- Currency
-L["Total"] = "Total"
-L["Currency"] = "Currency"
-L["Gold"] = "Gold"
+L["Total"] = true
+L["Currency"] = true
+L["Gold"] = true
 
 -- Runes
-L["Runes"] = "Runes"
+L["Runes"] = true
 L["Grizzby"] = true
-L["Dark Riders"] = "Dark Riders"
+L["Dark Riders"] = true
 L["No rune data"] = "No rune data, please log this character and open the character sheet to refresh"
-L["Chest"] = "Chest"
-L["Hands"] = "Hands"
-L["Waist"] = "Waist"
-L["Legs"] = "Legs"
-L["Feet"] = "Feet"
+L["Chest"] = true
+L["Hands"] = true
+L["Waist"] = true
+L["Legs"] = true
+L["Feet"] = true
 
 -- Books
-L["Books"] = "Books"
-L["Phase 2"] = "Phase 2"
+L["Books"] = true
+L["Phase 2"] = true
 
 -- Raids
-L["Raids"] = "Raids"
+L["Raids"] = true
 L["Blackfathom Deeps"] = true
 L["Gnomeregan"] = true
 
 -- PvP
-L["PvP"] = "PvP"
-L["Blood coins"] = "Blood coins"
-L["Ashenvale weekly"] = "Ashenvale weekly"
-L["Warsong Gulch"] = "Warsong Gulch"
-L["Arathi Basin"] = "Arathi Basin"
-L["Alterac Valley"] = "Alterac Valley"
+L["PvP"] = true
+L["Blood coins"] = true
+L["Ashenvale weekly"] = true
+L["Warsong Gulch"] = true
+L["Arathi Basin"] = true
+L["Alterac Valley"] = true
 
 -- Factions
-L["Factions"] = "Factions"
-L["ACA/DSL"] = "ACA/DSL"
+L["Factions"] = true
+L["ACA/DSL"] = true
 
 -- Professions
-L["Professions"] = "Professions"
-L["First primary"] = "First primary"
-L["Second primary"] = "Second primary"
-L["Alchemy"] = "Alchemy"
-L["Blacksmithing"] = "Blacksmithing"
-L["Enchanting"] = "Enchanting"
-L["Engineering"] = "Engineering"
-L["Herbalism"] = "Herbalism"
-L["Leatherworking"] = "Leatherworking"
-L["Mining"] = "Mining"
-L["Skinning"] = "Skinning"
-L["Tailoring"] = "Tailoring"
-L["Cooking"] = "Cooking"
-L["First Aid"] = "First Aid"
-L["Fishing"] = "Fishing"
+L["Professions"] = true
+L["First primary"] = true
+L["Second primary"] = true
+L["Alchemy"] = true
+L["Blacksmithing"] = true
+L["Enchanting"] = true
+L["Engineering"] = true
+L["Herbalism"] = true
+L["Leatherworking"] = true
+L["Mining"] = true
+L["Skinning"] = true
+L["Tailoring"] = true
+L["Cooking"] = true
+L["First Aid"] = true
+L["Fishing"] = true
 
 -- Config
 L["Show characters"] = true
 L["Show sections"] = true
 
 -- Minimap button
-L["Left click"] = "Left click"
-L["Open GUI"] = "Open GUI"
-L["Right click"] = "Right click"
-L["Open options"] = "Open options"
+L["Left click"] = true
+L["Open GUI"] = true
+L["Right click"] = true
+L["Open options"] = true
